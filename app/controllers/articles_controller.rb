1b71c6c58dde870597ce8d139f30d634ac83e166
遠藤大志 <taishi.domybest3607@gmail.com>
2021-03-11 14:47:54 +0900

    showページを作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
     # @articles = Article.find(6)
     @articles = Article.all
   end
+
+  def show
+   @article = Article.find(params[:id])
+  end
 end
